b1a4d92ae643da7382fc79c47718240365e27140
robert-w <RWinter85@gmail.com>
2024-04-16 14:01:07 -0400

    feat: created AppState to contain state passed in to routers

---
+++ src/state.rs
@@ -0,0 +1,17 @@
+use sqlx::{Pool, Postgres};
+
+use crate::db::postgres;
+
+#[derive(Clone)]
+pub struct AppState {
+    pub pool: Pool<Postgres>,
+}
+
+impl AppState {
+    pub async fn new() -> anyhow::Result<AppState> {
+        let connection_string = postgres::get_connection_string();
+        let pool = postgres::create_pool(&connection_string).await?;
+
+        Ok(AppState { pool })
+    }
+}
